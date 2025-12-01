8abf4348d8f7951673aaa2d0ce8c80d90efa8dcf
avaint <194505224+OmegaMasterXV@users.noreply.github.com>
2025-12-01 18:40:21 +0530

    updated socket config for prod

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -2,5 +2,7 @@ import { io } from "socket.io-client";
 import { baseUrl } from "./constants";
 
 export const createSocketConnection=()=>{
+    if(window.location==="localhost")
     return io(baseUrl);
+    return io("/",{path:"api/socket.io"})
 }
